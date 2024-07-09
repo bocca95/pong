6af86293511a1bced51bca9f04c90ec11d21bda1
Henrique <166140788+bocca95@users.noreply.github.com>
2024-07-09 00:18:49 -0300

    Pre Compiled Header

---
+++ pch.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Network.hpp>
+#include <SFML/Audio.hpp>
+#include <fstream>
+#include <iostream>
+#include <cmath>
+#include <sstream>
+#include <cstdlib>
+#include <ctime>
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
